935a0aae5bfa13088ec8e16621f67fe68974069e
antonia1210 <antonia.szalok@stud.ubbcluj.ro>
2025-09-08 14:01:55 +0300

    Login commit

---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login,  logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+            #messages.success(request, 'You are logged in')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'accounts/login.html')
+
+def home_view(request):
+    return render(request, 'accounts/home_page.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
